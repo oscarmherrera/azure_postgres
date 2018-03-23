c092a57090513c2b4f03a2c2587006f9e929c3e4
Oscar M Herrera <oscar@kss-inc.com>
2018-03-22 20:15:39 -0400

    Changed to Ubuntu and added repo key

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -97,7 +97,9 @@ install_postgresql_service() {
 	# Install PostgreSQL if it is not yet installed
 	if [ $(dpkg-query -W -f='${Status}' postgresql 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
-	  apt-get -y install postgresql=9.6* postgresql-contrib=9.6* postgresql-client=9.6*
+		add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
+		wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	  	apt-get -y install postgresql-9.6* postgresql-contrib-9.6* postgresql-client-9.6*
 	fi
 	
 	logger "Done installing PostgreSQL..."
