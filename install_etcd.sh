d84879d4d5e70c2b0b258a78b315031bc04c2fc6
Oscar M Herrera <oscar@kss-inc.com>
2018-05-03 23:15:45 -0400

    Fixed the if Command

--- install_etcd.sh
+++ install_etcd.sh
@@ -105,15 +105,15 @@ install_etcd_service() {
         cp ./etcd /usr/sbin
         cp ./etcdctl /usr/sbin 
         curl -L https://raw.githubusercontent.com/oscarmherrera/azure_postgres/master/etcd.template -o etcd.initd
-        if [$NODEID == 0  ]
+        if [$NODEID == 0  ] then
         sed -e "s/\${node0Name}/infra0/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA0/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
 
-        if [$NODEID == 1  ]
+        if [$NODEID == 1  ] then
         sed -e "s/\${node1Name}/infra1/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA1/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
 
-        if [$NODEID == 2  ]
+        if [$NODEID == 2  ] then
         sed -e "s/\${node2Name}/infra2/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA2/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
         #rm -rf ~/etcd_install
