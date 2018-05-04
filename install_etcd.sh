03094b8575564766f78abc16bf812e03f1ccd64e
Oscar M Herrera <oscar@kss-inc.com>
2018-05-03 22:58:05 -0400

    added multinode template replacement

--- install_etcd.sh
+++ install_etcd.sh
@@ -105,7 +105,9 @@ install_etcd_service() {
         cp ./etcd /usr/sbin
         cp ./etcdctl /usr/sbin 
         curl -L https://raw.githubusercontent.com/oscarmherrera/azure_postgres/master/etcd.template -o etcd.initd
-        sed -e "s/\${node0Name}/infra0/" -e "s/\${node0IP}/$INFRA0/" etcd.initd
+        sed -e "s/\${node0Name}/infra0/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA0/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
+        sed -e "s/\${node1Name}/infra1/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA1/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
+        sed -e "s/\${node2Name}/infra2/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA2/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
 
         #rm -rf ~/etcd_install
 	
