8d5ff4bef6de876265d23fc432e22fb001c1f5ca
Oscar M Herrera <oscar@kss-inc.com>
2018-05-05 19:41:07 -0400

    Added debug level for etcd

--- etcd/install_etcd.sh
+++ etcd/install_etcd.sh
@@ -107,15 +107,15 @@ install_etcd_service() {
         cp ./etcdctl /usr/sbin 
         curl -L https://raw.githubusercontent.com/oscarmherrera/azure_postgres/master/etcd/etcd.template -o etcd.initd
         if [[ "$NODEID" -eq 0 ]]; then
-        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${node0Name}/infra0/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA0/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
+        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${nodeName}/infra0/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA0/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
 
         if [[ "$NODEID" -eq 1 ]]; then
-        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${node1Name}/infra1/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA1/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
+        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${nodeName}/infra1/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA1/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
 
         if [[ "$NODEID" -eq 2 ]]; then
-        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${node2Name}/infra2/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA2/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
+        sed -i -e "s/\${CLUSTER_KEY}/${KEY}/" -e "s/\${nodeName}/infra2/"  -e "s/\${PORT}/2378/" -e "s/\${node0IP}/$INFRA2/" -e "s/\${infra0IP}/$INFRA0/" -e "s/\${infra1IP}/$INFRA1/" -e "s/\${infra2IP}/$INFRA2/" etcd.initd
         fi
 
 	      cp ~/etcd_install/etcd-v2.2.2-linux-amd64/etcd.initd /etc/init.d/etcd
